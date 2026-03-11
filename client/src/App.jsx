8074d5bda3fbe37ce94df7adc2c6e107686eb80b
Your Name <your-email@example.com>
2026-03-11 11:49:41 -0700

    imported  and add more routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,25 @@
-import SightingsList from './components/SightingsList';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+import SightingsList from './components/SightingsList';
+import SingleSighting from './components/SingleSighting';
+import NewSightingForm from './components/NewSightingForm';
+
+import IndividualsList from './components/IndividualsList';
+import SingleIndividual from './components/SingleIndividual';
+
+export default function App() {
   return (
-    <div>
-      <h1>Animal Sighting Tracker</h1>
-      <SightingsList />
-    </div>
+    <Router>
+      <Routes>
+        {/* Sightings */}
+        <Route path="/" element={<SightingsList />} />
+        <Route path="/sightings/:id" element={<SingleSighting />} />
+        <Route path="/new-sighting" element={<NewSightingForm />} />
+
+        {/* Individuals */}
+        <Route path="/individuals" element={<IndividualsList />} />
+        <Route path="/individuals/:id" element={<SingleIndividual />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
