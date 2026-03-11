40defa6c532c1b3f51f760970b1e57a9f7b15e6c
Your Name <your-email@example.com>
2026-03-10 21:16:37 -0700

    setting up state for messages and validation

--- client/src/components/NewSightingForm.jsx
+++ client/src/components/NewSightingForm.jsx
@@ -1,9 +1,30 @@
 import { useState } from 'react';
 
 export default function NewSightingForm() {
+  // State for each form field
+  const [individualId, setIndividualId] = useState('');
+  const [sightedAt, setSightedAt] = useState('');
   const [location, setLocation] = useState('');
+  const [healthy, setHealthy] = useState(true);
+  const [email, setEmail] = useState('');
 }
 
+// State for message
+
+const [error, setError] = useState(null);
+const [success, setSuccess] = useState(null);
+
+async function handleSubmit(e) {
+  e.preventDefault(); // prevents page reload
+
+  setError(null);
+  setSuccess(null);
+
+  if (!individualId || !sightedAt || !location || !email) {
+    setError('Please fill in all required fields.');
+    return;
+  }
+}
 return (
   <form>
     <h2>New Sighting</h2>
