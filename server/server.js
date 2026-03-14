907d806b468d7c5020504c45e5421f6a4d7045e7
Your Name <your-email@example.com>
2026-03-13 20:56:22 -0700

    README.md

--- server/server.js
+++ server/server.js
@@ -1,20 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-
-const sightingsRouter = require('./routes/sightings');
-const speciesRouter = require('./routes/species');
-const individualsRouter = require('./routes/individuals');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/sightings', sightingsRouter);
-app.use('/species', speciesRouter);
-app.use('/individuals', individualsRouter);
+const app = require('./app');
 
 const PORT = 3001;
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
